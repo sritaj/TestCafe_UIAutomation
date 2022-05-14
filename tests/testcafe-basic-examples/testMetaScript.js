f2aa1e7b4d5bf30260cd64f2142c52d46b3d8ea2
Sritaj <sritajp@gmail.com>
2022-05-14 20:38:17 +0530

    Added TestCafe example to demonstrate  usage of Meta scripts

---
+++ tests/testcafe-basic-examples/testMetaScript.js
@@ -0,0 +1,31 @@
+import { Selector } from "testcafe";
+
+fixture("Test demonstrating MetaTestData usage in TestCafe").page(
+	"https://devexpress.github.io/testcafe/example/"
+);
+
+const nameInputField = Selector("#developer-name");
+const submitButton = Selector("#submit-button");
+const articleHeader = Selector("#article-header");
+
+test.meta("severity", "p1")(
+	"Validate Scenario with Test MetaData usage - Example 1",
+	async (t) => {
+		await t
+			.typeText(nameInputField, "Sritaj")
+			.click(submitButton)
+			.expect(Selector(articleHeader).innerText)
+			.contains("Sritaj");
+	}
+);
+
+test.meta("severity", "p2")(
+	"Validate Scenario with Test MetaData usage - Example 2",
+	async (t) => {
+		await t
+			.typeText(nameInputField, "Anusha")
+			.click(submitButton)
+			.expect(Selector(articleHeader).innerText)
+			.contains("Anusha");
+	}
+);
