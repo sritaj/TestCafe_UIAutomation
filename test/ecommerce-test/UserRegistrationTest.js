6a3323ceceff222ec3d3487f6957f1dd5e5007fd
Sritaj <sritajp@gmail.com>
2022-05-16 01:11:35 +0530

    Added Test that utilizes functions from the POM pages

--- test/ecommerce-test/UserRegistrationTest.js
+++ test/ecommerce-test/UserRegistrationTest.js
@@ -15,13 +15,16 @@ test("Assert Home Page", async (t) => {
 	await t.expect(HomePage.subTitleHeader.exists).ok;
 });
 
-test("User Registration and Login Test", async (t) => {
+test("Assert User Registration and Login Test", async (t) => {
 	const firstName = faker.name.firstName();
 	const lastName = faker.name.lastName();
+	const email = faker.internet.email();
+	const password = "12345678";
 	await t
 		.click(HomePage.registerLink)
 		.expect(getPageURL())
 		.contains("register")
+		//Create New User
 		.click(RegisterPage.genderOption)
 		.typeText(RegisterPage.firstName, firstName)
 		.typeText(RegisterPage.lastName, lastName);
@@ -30,10 +33,56 @@ test("User Registration and Login Test", async (t) => {
 	await RegisterPage.selectMonth("March");
 	await RegisterPage.selectYear("1990");
 	await t
-		.typeText(RegisterPage.email, faker.internet.email())
-		.typeText(RegisterPage.password, "12345678")
-		.typeText(RegisterPage.confirmPassword, "12345678")
+		.typeText(RegisterPage.email, email)
+		.typeText(RegisterPage.password, password)
+		.typeText(RegisterPage.confirmPassword, password)
 		.click(RegisterPage.registerButton)
 		.expect(RegisterPage.successfullMessage.exists)
+		.ok()
+		//Logout and Login
+		.click(HomePage.logOutLink)
+		.click(HomePage.logInLink)
+		.expect(LoginPage.accountHeader.exists)
+		.ok()
+		.typeText(LoginPage.emailInput, email)
+		.typeText(LoginPage.passwordInput, password)
+		.click(LoginPage.loginButton)
+		//Navigate to Customer Page and Verify Orders
+		.click(HomePage.myAccountLink)
+		.expect(CustomerPage.ordersLink.exists)
+		.ok()
+		.click(CustomerPage.ordersLink)
+		.expect(CustomerPage.noOrdersLabel.exists)
 		.ok();
+});
+
+test("Assert User Registration and Login Test - Code Refactored", async (t) => {
+	const firstName = faker.name.firstName();
+	const lastName = faker.name.lastName();
+	const email = faker.internet.email();
+	const password = "12345678";
+
+	await HomePage.clickOnRegisterLink();
+	await t.expect(getPageURL()).contains("register");
+	//Create New User
+	await RegisterPage.registerUser(
+		firstName,
+		lastName,
+		"10",
+		"March",
+		"1990",
+		email,
+		password
+	);
+	await t.expect(RegisterPage.successfullMessage.exists).ok();
+	//Logout and Login
+	await HomePage.clickOnLogout();
+	await HomePage.clickOnLogin();
+	await t.expect(LoginPage.accountHeader.exists).ok();
+	await LoginPage.loginToAccount(email, password);
+	//Navigate to Customer Page and Verify Orders
+	await HomePage.clickOnMyAccount();
+	await t.expect(CustomerPage.ordersLink.exists).ok();
+	await CustomerPage.clickOnOrdersLink();
+	await t.expect(CustomerPage.noOrdersLabel.exists).ok();
 });
