36586a8bbd4c198591c286b75715806048dd3819
Sritaj <sritajp@gmail.com>
2022-05-15 20:49:30 +0530

    Added functions for setting Month and Year

--- pages/RegisterPage.js
+++ pages/RegisterPage.js
@@ -5,9 +5,9 @@ class RegisterPage {
 		this.genderOption = Selector("#gender-male");
 		this.firstName = Selector("#FirstName");
 		this.lastName = Selector("#LastName");
-		this.dateOfBirthDayList = Selector("select#DateOfBirthDay");
-		this.dateOfBirthMonthList = Selector("select#DateOfBirthMonth");
-		this.dateOfBirthYearList = Selector("select#DateOfBirthYear");
+		this.dateOfBirthDayList = Selector("select[name='DateOfBirthDay']");
+		this.dateOfBirthMonthList = Selector("select[name='DateOfBirthMonth']");
+		this.dateOfBirthYearList = Selector("select[name='DateOfBirthYear']");
 		this.email = Selector("#Email");
 		this.password = Selector("#Password");
 		this.confirmPassword = Selector("#ConfirmPassword");
@@ -21,6 +21,18 @@ class RegisterPage {
 		const dayOption = this.dateOfBirthDayList.find("option");
 
 		await t.click(this.dateOfBirthDayList).click(dayOption.withText(day));
+	}
+
+	async selectMonth(month) {
+		const monthOption = this.dateOfBirthMonthList.find("option");
+
+		await t.click(this.dateOfBirthMonthList).click(monthOption.withText(month));
+	}
+
+	async selectYear(year) {
+		const yearOption = this.dateOfBirthYearList.find("option");
+
+		await t.click(this.dateOfBirthYearList).click(yearOption.withText(year));
 	}
 }
 
